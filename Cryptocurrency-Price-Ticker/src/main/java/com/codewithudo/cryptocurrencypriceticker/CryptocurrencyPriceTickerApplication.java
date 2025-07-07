a1185bef97282a5e8cb17489f20e54df207c092f
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-07 21:49:02 +0100

    Price Ticker Folder Structure

---
+++ Cryptocurrency-Price-Ticker/src/main/java/com/codewithudo/cryptocurrencypriceticker/CryptocurrencyPriceTickerApplication.java
@@ -0,0 +1,13 @@
+package com.codewithudo.cryptocurrencypriceticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyPriceTickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocurrencyPriceTickerApplication.class, args);
+    }
+
+}

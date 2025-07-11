9251cb06634f7172dba1cda13cde9702d98fe36b
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-11 17:33:37 +0100

    QuidaxService class

--- src/main/java/com/codewithudo/cryptocurrencypriceticker/service/QuidaxService.java
+++ src/main/java/com/codewithudo/cryptocurrencypriceticker/service/QuidaxService.java
@@ -1,4 +1,40 @@
 package com.codewithudo.cryptocurrencypriceticker.service;
 
+import com.codewithudo.cryptocurrencypriceticker.dto.Ticker;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
 public class QuidaxService {
+
+    private static final String QUIDAX_API_BASE_URL = "https://api.quidax.com";
+    private final RestTemplate restTemplate;
+
+    public QuidaxService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public Map<String, Ticker> getTickers() {
+        String url = QUIDAX_API_BASE_URL + "/api/v1/tickers";
+
+        // We use ParameterizedTypeReference to help Jackson understand the complex return type:
+        // a Map of Strings to Tickers.
+        ResponseEntity<Map<String, Ticker>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+        return response.getBody();
+    }
+
+    public Ticker getTicker(String market) {
+        String url = QUIDAX_API_BASE_URL + "/api/v1/tickers/" + market;
+        return restTemplate.getForObject(url, Ticker.class);
+    }
 }
